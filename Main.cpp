202803e4cb4566698f7bc44478f45828da4349c5
André Pita <andrempita@gmail.com>
2026-03-18 21:43:17 +0000

    added input limit

--- Main.cpp
+++ Main.cpp
@@ -6,16 +6,18 @@
 using namespace std;
 
 int main() {
-    // Read one line at a time; each line must contain exactly 3 integers.
+    // Read one line at a time, max 20
+    int n = 0;
     string line;
     vector<array<int, 3>> trikits;
     // Get one array (3 integers) per line until EOF (ctrl+Z + Enter)
-    while (getline(cin, line)) {
+    while (getline(cin, line) && n < 20) {
         istringstream iss(line);
         array<int, 3> trikit;
 
         if ((iss >> trikit[0] >> trikit[1] >> trikit[2]) && iss.eof()) {
             trikits.push_back(trikit);
+            n++;
         } else {
             continue; // Skip lines that don't contain exactly 3 integers
         }
